8e243d157ac1258509028ea8b1d31a8a06a89c81
anudeepmuppalla1729 <anudeepmuppalla@gmail.com>
2025-03-19 23:19:31 +0530

    Made Changes in Schema of requests (added user options , added approval and delivering in status enum

--- server/models/requests.js
+++ server/models/requests.js
@@ -6,11 +6,14 @@ const requestSchema = new mongoose.Schema({
   issueDescription: String,
   status: { 
     type: String, 
-    enum: ["Pending", "Approved", "Picked Up", "In Repair", "Completed", "Denied"],
+    enum: ["Pending", "Approved", "Picked Up", "Approval", "In Repair", "Delivering", "Paid", "Denied"],
     default: "Pending" 
   },
   assignedAgent: { type: mongoose.Schema.Types.ObjectId, ref: "Agent" },
   selectedServiceCenter: { type: mongoose.Schema.Types.ObjectId, ref: "ServiceCenter" },
+  affordable: { type: Map, of: Number},
+  goodToHave : { type: Map, of: Number},
+  niceToHave: { type: Map, of: Number},
   amountDue: { type: Number, default: 0 }, // Amount to be paid by the customer
   isPaid: { type: Boolean, default: false }, // Tracks if payment is completed
   createdAt: { type: Date, default: Date.now }
