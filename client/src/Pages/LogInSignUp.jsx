e3b9510e51cf818793efde685282d9c5e4b5499a
AviBapatu <avinashreddybapatu2006@gmail.com>
2025-03-25 16:31:06 +0530

    Login and a Part of profile setup page

---
+++ client/src/Pages/LogInSignUp.jsx
@@ -0,0 +1,96 @@
+import React, {useState} from 'react';
+import {useNavigate} from "react-router-dom";
+import axios from 'axios';
+import Logo from '../assets/Logo.png'
+
+function LogInSignUp() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isRegistering, setIsRegistering] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+
+      if (!email || !password) {
+        alert('Please enter both email and password');
+        return;
+      }
+
+      if (isRegistering) {
+        const response = await axios.post("http://localhost:3000/api/auth/user/register", {
+          email,
+          password
+        });
+
+        const token = response.data.token;
+        localStorage.setItem("token", token);
+        alert("Registration Successful!");
+        navigate('/name-phone-photo');
+      } else {
+        const response = await axios.post("http://localhost:3000/api/auth/user/login", {
+          email,
+          password
+        });
+
+        const token = response.data.token;
+        localStorage.setItem("token", token);
+        alert("LogIn Successful!");
+        navigate('/Dashboard');
+      }
+
+    } catch (error) {
+      alert(isRegistering ? "Registration Failed" : "LogInSignUp Failed");
+      console.log(error);
+    }
+  };
+
+  return (
+      <>
+        <div>
+          <img src={Logo} alt="AAmigo's Logo" />
+          {isRegistering ? <h3>Sign Up</h3> : <h3>Log In</h3>}
+          <form onSubmit={handleSubmit}>
+            <input
+                type="email"
+                placeholder={"Email Id"}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder={"Password"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">
+              {isRegistering ? "Sign Up" : "Log In"}
+            </button>
+          </form>
+        </div>
+
+        <div>
+          <h1>Hello, AAmigo's</h1>
+          {isRegistering ? (
+              <>
+                <h4>Already A User?</h4>
+                <button type="button" onClick={() => setIsRegistering(false)}>
+                  Log In
+                </button>
+              </>
+          ) : (
+              <>
+                <h4>Not A User?</h4>
+                <button type="button" onClick={() => setIsRegistering(true)}>
+                  Sign Up
+                </button>
+              </>
+          )}
+        </div>
+      </>
+  );
+}
+
+
+export default LogInSignUp;
---
+++ client/src/Pages/profileSetup/NamePhonePhoto.jsx
@@ -0,0 +1,71 @@
+import React, {useState} from 'react';
+import {useNavigate} from "react-router-dom";
+import axios from 'axios';
+import Logo from "../../assets/Logo.png"
+import {preview} from "vite";
+
+function NamePhonePhoto() {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('')
+  const [profilePic, setProfilePic] = useState(null)
+  const [previewUrl, setPreviewUrl] = useState('')
+
+  const handleProfilePicChange = (e) => {
+    const imageFile = e.target.files[0]
+    if (!imageFile) {
+      alert("No file selected");
+    }
+    setProfilePic(imageFile)
+    setPreviewUrl(URL.createObjectURL(imageFile));
+  }
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      if (!profilePic) {
+        alert("Please select a profile Picture")
+        return;
+      }
+      if (!name || !phone) {
+        alert("Please Fill both the name and password")
+        return;
+      }
+
+      const formData = new FormData();
+      formData.append("name", name);
+      formData.append("phone", phone);
+      formData.append("profilePic", profilePic);
+
+      try {
+        const response = await axios.post("http://localhost:3000/api/setup/user/setupProfile", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            "Authorization": `Bearer ${localStorage.getItem("token")}`
+          }
+        });
+        console.log("Details are saved.", response.data);
+      } catch (error) {
+        console.error("Error uploading profile:", error);
+      }
+
+    };
+    return (
+        <>
+          <img src={Logo} alt="AAMigo's Logo"/>
+          <form onSubmit={handleSubmit} >
+            <input type="file" accept={"image/*"} onClick={handleProfilePicChange}/>
+            {previewUrl && <img src={previewUrl} alt="Profile Pic Preview "/>}
+
+            <label htmlFor="nameInput">Name</label>
+            <input type="text" id={"nameInput"} onChange={(e) => setName(e.target.value)}
+                   placeholder={"Enter Name..."}/>
+
+            <label htmlFor={"phoneInput"}>Contact no.</label>
+            <input type="number" id={"phoneInput"} onChange={(e) => setPhone(e.target.value)}
+                   placeholder={"Enter Contact Number..."}/>
+
+            <button type={"submit"}>Next</button>
+          </form>
+        </>
+    )
+}
+export default NamePhonePhoto;
